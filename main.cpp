de7cde0eae240d375839b5b04ad74e68ff17d6db
bartlej <bjarzembinski@yahoo.pl>
2019-06-19 00:57:59 +0200

    after closing video go back to menu and add possibility of exit for menu

--- main.cpp
+++ main.cpp
@@ -10,7 +10,9 @@
 #include <opencv2/highgui.hpp>
 #include <opencv2/video.hpp>
 
-#define ESC 27					
+#define ESC 27
+#define KAMERA 49
+#define FILM 50
 
 using namespace cv;
 using namespace std;
@@ -26,8 +28,21 @@ int menu() {
 	cout << "1. KAMERA" << endl;
 	cout << "2. FILM" << endl;
 	cout << endl;
-	cout << "Twoj wybor:" << endl;
-	cin >> choice;
+	cout << "Wyjscie: ESC" << endl;
+	choice = _getch();
+	cout << endl;
+	
+	if (choice == KAMERA) {
+		cout << "Wybrano tryb: KAMERA" << endl;
+	} else if (choice == FILM){
+		cout << "Wybrano tryb: FILM" << endl;
+	} else if (choice == ESC) {
+		cout << "Zamykam program, nacisnij dowolny klawisz, zeby kontynuowac" << endl;
+	}
+	else {
+		cout << "Niepoprawnie wybrano tryb, nacisnij dowolny klawisz, zeby kontynuowac" << endl;
+	}
+	_getch();
 	cout << endl;
 	return choice;
 }
@@ -97,112 +112,119 @@ int main() {
 	Ptr<BackgroundSubtractorMOG2> pMOG2;
 	pMOG2 = createBackgroundSubtractorMOG2();
 
-	// wybor zrodla obrazu
-	int mode = menu();
+	while (true) {
+		// wybor zrodla obrazu
+		int mode = menu();
 
-	switch (mode) {
-		case 1: {
-			cap.open(0); //otwarcie kamery
-			fps = 50;
-			break;
-		}
-		case 2: {
-			string path = videoChoice();
-			cap.open(path); //wczytanie pliku wideo
-			fps = cap.get(CAP_PROP_FPS);
-			break;
-		}
-		default: {
-			cout << "Niepoprawnie wybrano tryb." << endl; //komunikat o bledzie i zamkniecie programu
-			return -1;
-		}
-	}
-
-	//komunikat o poprawnosci inicjalizacji przechwytywania
-	if (!cap.isOpened()) {
-		cout << endl << "Inicjalizacja przechwytywania zakonczona niepowodzeniem." << endl;
-	}
-	else {
-		cout << endl << "Inicjalizacja przechwytywania zakonczona powodzeniem." << endl;
-	}
-
-	//stworzenie okna dla wyswietlanego obrazu
-	namedWindow("Fire Detection", WINDOW_AUTOSIZE);
-	namedWindow("Parameters", WINDOW_AUTOSIZE);
-	createTrackbar("Threshold", "Parameters", &threshold, 100);
-	createTrackbar("Min Area", "Parameters", &minArea, 10000);
-
-	while (cap.read(frame)) {
-		//kopia klatki 
-		fireDetectionFrame = frame.clone();
-
-		//inicjalizacja maski ognia
-		if (initialized == false) {
-			fireMask = Mat(frame.rows, frame.cols, CV_8UC1);
-			initialized = true;
-		}
-		
-		//utworzenie maski ruchu 
-		pMOG2->apply(frame, motionMask);
-		erode(motionMask, motionMask, Mat(), Point(-1, -1), 2);
-		dilate(motionMask, motionMask, Mat(), Point(-1, -1), 1);
-
-		//konwersja obrazu do przestrzni YCrCb
-		cvtColor(frame, fireFrameYCrCb, COLOR_BGR2YCrCb);
-
-		//wyekstrahowanie wartosci poszczegolnych kanalow
-		extractChannel(fireFrameYCrCb, fireFrameY, 0);
-		extractChannel(fireFrameYCrCb, fireFrameCr, 1);
-		extractChannel(fireFrameYCrCb, fireFrameCb, 2);
-
-		//obliczenie wartosci srednich dla poszczegolnych kanalow
-		meanYCrCb = mean(frame);
-		int meanY = meanYCrCb[0];
-		int meanCr = meanYCrCb[1];
-		int meanCb = meanYCrCb[2];
-
-		//utworzenie wektora pikseli pozaru
-		vector<Point> firePixels;
-
-		//sprawdzenie dla kazdego piksela czy jest na pierwszym planie i czy spelnia zalozenia koloru ognia
-		for (int y = 0; y < fireFrameYCrCb.rows; y++) {
-			for (int x = 0; x < fireFrameYCrCb.cols; x++) {
-				if (motionMask.at<uchar>(y, x) > 0 && isFireColor(fireFrameY, fireFrameCr, fireFrameCb, y, x, threshold, meanY, meanCr, meanCb)) {
-					fireMask.at<uchar>(y, x) = 255;
-					//dodanie piksela do wektora pikseli pozaru
-					firePixels.push_back(Point(x, y));
-				}
-				else {
-					fireMask.at<uchar>(y, x) = 0;
-				}
+		switch (mode) {
+			case KAMERA: {
+				cap.open(0); //otwarcie kamery
+				fps = 50;
+				break;
+			}
+			case FILM: {
+				string path = videoChoice();
+				cap.open(path); //wczytanie pliku wideo
+				fps = cap.get(CAP_PROP_FPS);
+				break;
+			}
+			case ESC: {
+				return 0;
+			}
+			default: {
+				return 1;
 			}
 		}
-		
-		//narysowanie prostokata wskazujacego obszar pozaru
-		boundRect = boundingRect(firePixels);
-		if (boundRect.area() > minArea) {
-			rectangle(fireDetectionFrame, boundRect.tl(), boundRect.br(), CV_RGB(0, 255, 0), 2);
+
+		//komunikat o poprawnosci inicjalizacji przechwytywania
+		if (!cap.isOpened()) {
+			cout << endl << "Inicjalizacja przechwytywania zakonczona niepowodzeniem." << endl;
+		}
+		else {
+			cout << endl << "Inicjalizacja przechwytywania zakonczona powodzeniem." << endl;
 		}
 
-		try {
-			//wyswietlenie obrazow
-			imshow("Fire Detection", fireDetectionFrame);
-			//imshow("maska ruchu", motionMask);
-			//imshow("maska ognia", fireMask);
-			//imshow("Y", fireFrameY);
-			//imshow("Cr", fireFrameCr);
-			//imshow("Cb", fireFrameCb);
-		}
+		//stworzenie okna dla wyswietlanego obrazu
+		namedWindow("Fire Detection", WINDOW_AUTOSIZE);
+		namedWindow("Parameters", WINDOW_AUTOSIZE);
+		createTrackbar("Threshold", "Parameters", &threshold, 100);
+		createTrackbar("Min Area", "Parameters", &minArea, 10000);
 
-		catch (Exception &e) {
-			cout << e.what() << endl;
-			return 1;
+		while (cap.read(frame)) {
+			//kopia klatki 
+			fireDetectionFrame = frame.clone();
+
+			//inicjalizacja maski ognia
+			if (initialized == false) {
+				fireMask = Mat(frame.rows, frame.cols, CV_8UC1);
+				initialized = true;
+			}
+
+			//utworzenie maski ruchu 
+			pMOG2->apply(frame, motionMask);
+			erode(motionMask, motionMask, Mat(), Point(-1, -1), 2);
+			dilate(motionMask, motionMask, Mat(), Point(-1, -1), 1);
+
+			//konwersja obrazu do przestrzni YCrCb
+			cvtColor(frame, fireFrameYCrCb, COLOR_BGR2YCrCb);
+
+			//wyekstrahowanie wartosci poszczegolnych kanalow
+			extractChannel(fireFrameYCrCb, fireFrameY, 0);
+			extractChannel(fireFrameYCrCb, fireFrameCr, 1);
+			extractChannel(fireFrameYCrCb, fireFrameCb, 2);
+
+			//obliczenie wartosci srednich dla poszczegolnych kanalow
+			meanYCrCb = mean(frame);
+			int meanY = meanYCrCb[0];
+			int meanCr = meanYCrCb[1];
+			int meanCb = meanYCrCb[2];
+
+			//utworzenie wektora pikseli pozaru
+			vector<Point> firePixels;
+
+			//sprawdzenie dla kazdego piksela czy jest na pierwszym planie i czy spelnia zalozenia koloru ognia
+			for (int y = 0; y < fireFrameYCrCb.rows; y++) {
+				for (int x = 0; x < fireFrameYCrCb.cols; x++) {
+					if (motionMask.at<uchar>(y, x) > 0 && isFireColor(fireFrameY, fireFrameCr, fireFrameCb, y, x, threshold, meanY, meanCr, meanCb)) {
+						fireMask.at<uchar>(y, x) = 255;
+						//dodanie piksela do wektora pikseli pozaru
+						firePixels.push_back(Point(x, y));
+					}
+					else {
+						fireMask.at<uchar>(y, x) = 0;
+					}
+				}
+			}
+
+			//narysowanie prostokata wskazujacego obszar pozaru
+			boundRect = boundingRect(firePixels);
+			if (boundRect.area() > minArea) {
+				rectangle(fireDetectionFrame, boundRect.tl(), boundRect.br(), CV_RGB(0, 255, 0), 2);
+			}
+
+			try {
+				//wyswietlenie obrazow
+				imshow("Fire Detection", fireDetectionFrame);
+				//imshow("maska ruchu", motionMask);
+				//imshow("maska ognia", fireMask);
+				//imshow("Y", fireFrameY);
+				//imshow("Cr", fireFrameCr);
+				//imshow("Cb", fireFrameCb);
+			}
+
+			catch (Exception &e) {
+				cout << e.what() << endl;
+				return 1;
+			}
+			if (waitKey(1000 / fps) == ESC) {
+				break;
+			}
 		}
-		if (waitKey(1000 / fps) == ESC) {
-			break;
-		}
+		//zamkniecie wszystkich okien i zwolnienie pamieci
+		cap.release();
+		destroyAllWindows();
+		system("cls");
 	}
-
 	//zamkniecie wszystkich okien i zwolnienie pamieci
 	cap.release();
 	destroyAllWindows();
